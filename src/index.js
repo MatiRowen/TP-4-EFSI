b4413f0d3d671fa6577623c9e8c1c7a1c11fc591
Matias Rowensztein <92543676+MatiRowen@users.noreply.github.com>
2022-07-04 10:05:21 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NavBar from './components/NavBar';
+import Carousel from './components/Carousel';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+import inscripcion from './inscipcion.jpeg';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <NavBar />
+    <Carousel />
+    <Cards 
+          titulo="Inscripción ciclo lectivo 2021 - Nivel Secundario"
+          texto="Por la gran cantidad de solicitudes, debimos cerrar la inscripción al curso de ingreso 2020, para iniciar en el ciclo
+          2021 del nivel secundario de nuestra Escuela. ¡Muchas gracias a todos por confiar en nuestro proyecto educativo!"
+          img= {inscripcion}
+    />
+    <Footer />
+  </React.StrictMode>
+);
